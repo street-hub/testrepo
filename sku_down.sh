05138288df910932cd4996d821d9653c88cdd21b
colin.dh <76595041+street-hub@users.noreply.github.com>
2022-07-15 16:36:57 +0900

    Update sku_down.sh

--- sku_down.sh
+++ sku_down.sh
@@ -1,7 +1,54 @@
+#!/bin/bash
 
+# Login with Service Principal
+echo "--------------------------------------------------------------------------------"
+printf '%40s\n' "Login with Service Principal"
+echo "--------------------------------------------------------------------------------"
 
-az login --service-principal -u 96ca8a76-2de5-4b0f-b799-caeb57eee698 --password JLU8Q~e4CJ92kyMm8nrYggzhj8onzxgc34~_tc5Q --tenant a74b5451-a280-4768-a5cd-cb47af153c71
+colin_app_id=$1
+colin_app_secret=$2
+colin_tenant_id=$3
+colin_subscription_id=$4
 
-az account set --subscription e415a38a-f61b-4917-b57f-e14b3b34e719
+az login --service-principal -u $colin_app_id -p $colin_app_secret --tenant $colin_tenant_id
+az account set --subscription $colin_subscription_id
 
-az vm resize --resource-group testchoi --name auto-test-vm --size Standard_D2s_v3
+# Check available VM Size 
+echo "--------------------------------------------------------------------------------"
+printf '%40s\n' "Check available VM Size"
+echo "--------------------------------------------------------------------------------"
+
+resource_group=$5
+vm_name=$6
+target_size=$7 
+
+isSizeAvailable=$(az vm list-vm-resize-options --resource-group $resource_group --name $vm_name --output table)
+if [ -z $isSizeAvailable ] # Empty == Unvailable
+then
+    echo "================================================================================"
+    printf '%40s\n' "$target_size size is Unavailable"
+    echo "================================================================================"
+    exit 1;
+fi
+echo "--------------------------------------------------------------------------------"
+printf '%40s\n' "$target_size size is Available"
+echo "--------------------------------------------------------------------------------"
+
+# Start VM Resize
+echo "--------------------------------------------------------------------------------"
+printf '%40s\n' "VM Size Change Start"
+echo "--------------------------------------------------------------------------------"
+az vm resize --resource-group $resource_group --name $vm_name --size $target_size
+
+# 1 == False / 0 == True
+isSizeChanged=$?
+if [ isSizeChanged == 1 ]
+then
+    echo "================================================================================"
+    printf '%40s\n' " Error Occured, Require admin check. "
+    echo "================================================================================"
+    exit 1;
+fi
+echo "--------------------------------------------------------------------------------"
+printf '%40s\n' "Job Completed."
+echo "--------------------------------------------------------------------------------"
